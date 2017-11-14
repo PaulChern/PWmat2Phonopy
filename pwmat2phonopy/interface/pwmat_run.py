f937ad36209e7d3b76c211eb6a05e01a99f4fdb6
PaulChern <cpiphy2011@gmail.com>
2017-11-14 16:21:18 +0800

    now read pwmat2phonopy.in

--- pwmat2phonopy/interface/pwmat_run.py
+++ pwmat2phonopy/interface/pwmat_run.py
@@ -46,8 +46,11 @@ class InputParser(object):
                 nodes = line.split()[0:2]
                 self._confs['nodes'] = '    '.join(nodes)
 
-    def get_configures(self):
-        return self._confs
+    def get_configures(self, key=None):
+        if key == None:
+            return self._confs
+        else:
+            return self._confs[key]
 
     def set_configures(self, key, elem):
         self._confs[key.lower()] = elem
@@ -99,68 +102,31 @@ def creat_post_process_script(num_forces):
     with open('plot_phonon.sh', 'w') as w:
         w.write("\n".join(lines))
 
-def creat_phonopy_conf(DIM, BAND, BAND_LABELS, BAND_POINTS, unit, MP, FPITCH, SIGMA):
-    if unit == '0': # THz
-        FREQUENCY_CONVERSION_FACTOR = '15.633302'
-    elif unit == '1': # cm^-1
-        FREQUENCY_CONVERSION_FACTOR = '521.47083'
-    elif unit == '2': # eV
-        FREQUENCY_CONVERSION_FACTOR = '6.46541380e-2'
-    elif unit == '3': # meV
-        FREQUENCY_CONVERSION_FACTOR = '6.46541380e1'
-    else:
-        FREQUENCY_CONVERSION_FACTOR = '15.633302'
-
-    lines = []
-    lines.append('DIM = '+DIM)
-    lines.append('PRIMITIVE_AXIS = 1.0 0.0 0.0  0.0 1.0 0.0  0.0 0.0 1.0')
-    lines.append('BAND = '+BAND)
-    lines.append('BAND_LABELS = '+BAND_LABELS)
-    lines.append('BAND_POINTS = '+BAND_POINTS)
-    lines.append('FREQUENCY_CONVERSION_FACTOR = '+FREQUENCY_CONVERSION_FACTOR)
-    lines.append('MP = '+MP)
-    lines.append('DOS = TRUE')
-    lines.append('FPITCH = '+FPITCH)
-    lines.append('SIGMA = '+SIGMA)
-    lines.append('')
-
-    with open('band_dos.conf', 'w') as w:
-        w.write("\n".join(lines))
-
-def creat_pwmat2phonopy_input():
-    lines = []
-    lines.append('nodes = 1    1                                           # node1 node2 for pwmat parallel configuration')
-    lines.append('DIM = 2 2 2                                              # supercell dimension which will be used to make displacements')
-    lines.append('PRIMITIVE_AXIS = 1.0 0.0 0.0  0.0 1.0 0.0  0.0 0.0 1.0   # ')
-    lines.append('BAND = 0.0 0.0 0.0  0.5 0.0 0.0                          # special q points in Brillioun zone')
-    lines.append('BAND_LABELS = \Gamma  X                                  # labels of the special q points')
-    lines.append('BAND_POINTS = 101                                        # number of q-point along the high symmetry path')
-    lines.append('FREQUENCY_CONVERSION_FACTO = THz                         # unit of the frequency')
-    lines.append('DOS = TRUE                                               # switch to the DOS calculation')
-    lines.append('MP = 41 41 41                                            # q-mesh for the DOS calculation')
-    lines.append('FPITCH = 0.1                                             # frequency interval for DOS calculation')
-    lines.append('SIGMA = 0.1                                              # smearing width for DOS calculation')
-    lines.append('')
-
-    with open('pwmat2phonopy.in', 'w') as w:
-        w.write("\n".join(lines))
-
 # Parse pwmat2phonopy.in
 class pwmat2phonopyParser(object):
-    def __init__(self, filename=None):
-        self._confs = {'nodes':{'val':'1    1                                           ', 'comm':'# node1 node2 for pwmat parallel configuration'},
-                       'DIM':{'val':'2 2 2                                              ', 'comm':'# supercell dimension which will be used to make displacements'},
-                       'PRIMITIVE_AXIS':{'val':'1.0 0.0 0.0  0.0 1.0 0.0  0.0 0.0 1.0   ', 'comm':'# the primitive cell for building the dynamical matrix'},
-                       'BAND':{'val':'0.0 0.0 0.0  0.5 0.0 0.0                          ', 'comm':'# special q points in Brillioun zone'},
-                       'BAND_LABELS':{'val':'\Gamma  X                                  ', 'comm':'# labels of the special q points'},
-                       'BAND_POINTS':{'val':'101                                        ', 'comm':'# number of q-point along the high symmetry path'},
-                       'FREQUENCY_CONVERSION_FACTOR':{'val':'THz                        ', 'comm':'# unit of the frequency'},
-                       'DOS':{'val':'TRUE                                               ', 'comm':'# switch to the DOS calculation'},
-                       'MP':{'val':'41 41 41                                            ', 'comm':'# q-mesh for the DOS calculation'},
-                       'FPITCH':{'val':'0.1                                             ', 'comm':'# frequency interval for DOS calculation'},
-                       'SIGMA':{'val':'0.1                                              ', 'comm':'# smearing width for DOS calculation'}
+    def __init__(self, filename=None, etotinput=None):
+        if etotinput is not None:
+            nodes = etotinput.get_configures('nodes')
+            mp_n123 = etotinput.get_configures('mp_n123')
+        else:
+            nodes = '1    1'
+            mp_n123 = '1 1 1 0 0 0'
+
+        self._confs = {'nodes':{'val':nodes+'                                           ', 'comm':'# node1 node2 for pwmat parallel configuration'},
+                       'wall_time':{'val':'1000:00:00                                   ', 'comm':'# wall time for the queue system (torque): hours:minutes:seconds'},
+                       'mp_n123':{'val':mp_n123+'                                ', 'comm':'# k-mesh for the supercell calculations; it is '+mp_n123+' for the unitcell calculation'},
+                       'dim':{'val':'2 2 2                                              ', 'comm':'# supercell dimension which will be used to make displacements'},
+                       'primitive_axis':{'val':'1.0 0.0 0.0  0.0 1.0 0.0  0.0 0.0 1.0   ', 'comm':'# the primitive cell for building the dynamical matrix'},
+                       'band':{'val':'0.0 0.0 0.0  0.5 0.0 0.0                          ', 'comm':'# special q points in Brillioun zone'},
+                       'band_labels':{'val':'\Gamma  X                                  ', 'comm':'# labels of the special q points'},
+                       'band_points':{'val':'101                                        ', 'comm':'# number of q-point along the high symmetry path'},
+                       'frequency_conversion_factor':{'val':'THz                        ', 'comm':'# unit of the frequency:"THz", "cm^-1", "eV", "meV"'},
+                       'dos':{'val':'TRUE                                               ', 'comm':'# switch to the DOS calculation'},
+                       'mp':{'val':'41 41 41                                            ', 'comm':'# q-mesh for the DOS calculation'},
+                       'fpitch':{'val':'0.1                                             ', 'comm':'# frequency interval for DOS calculation'},
+                       'sigma':{'val':'0.1                                              ', 'comm':'# smearing width for DOS calculation'}
                       }
-        self._keywords = ['nodes', 'DIM', 'PRIMITIVE_AXIS', 'BAND', 'BAND_LABELS', 'BAND_POINTS', 'FREQUENCY_CONVERSION_FACTOR', 'DOS', 'MP', 'FPITCH', 'SIGMA']
+        self._keywords = ['nodes', 'wall_time', 'mp_n123', 'dim', 'primitive_axis', 'band', 'band_labels', 'band_points', 'frequency_conversion_factor', 'dos', 'mp', 'fpitch', 'sigma']
         if filename is not None:
             self.read_input(filename) # store data in self._confs
 
@@ -183,8 +149,11 @@ class pwmat2phonopyParser(object):
                 right_value, comm = right.split('#')
                 self._confs[left.lower()] = {'val':right_value, 'comm':comm}
 
-    def get_configures(self):
-        return self._confs
+    def get_configures(self, key=None):
+        if key == None:
+            return self._confs
+        else:
+            return self._confs[key]
 
     def set_configures(self, key, elem):
         self._confs[key.lower()]['val'] = elem
@@ -198,12 +167,41 @@ class pwmat2phonopyParser(object):
 
         confs = self._confs
         lines = []
-        lines.append(confs['nodes'])
         for key in self._keywords:
-            lines.append(" = ".join([key, confs[key]['val']])+' '+confs[key]['comm'])
+            lines.append(" = ".join([key.upper(), confs[key]['val']])+' '+confs[key]['comm'])
         lines.append('')
 
         return lines
+
+    def creat_phonopy_conf(self):
+        confs = self._confs
+        unit = confs['frequency_conversion_factor']['val'].strip()
+        if unit == 'THz': # THz
+            FREQUENCY_CONVERSION_FACTOR = '15.633302'
+        elif unit == 'cm^-1': # cm^-1
+            FREQUENCY_CONVERSION_FACTOR = '521.47083'
+        elif unit == 'eV': # eV
+            FREQUENCY_CONVERSION_FACTOR = '6.46541380e-2'
+        elif unit == 'meV': # meV
+            FREQUENCY_CONVERSION_FACTOR = '6.46541380e1'
+        else:
+            FREQUENCY_CONVERSION_FACTOR = '15.633302'
+    
+        lines = []
+        lines.append('DIM = '+confs['dim']['val'].strip())
+        lines.append('PRIMITIVE_AXIS = '+confs['primitive_axis']['val'].strip())
+        lines.append('BAND = '+confs['band']['val'].strip())
+        lines.append('BAND_LABELS = '+confs['band_labels']['val'].strip())
+        lines.append('BAND_POINTS = '+confs['band_points']['val'].strip())
+        lines.append('FREQUENCY_CONVERSION_FACTOR = '+FREQUENCY_CONVERSION_FACTOR)
+        lines.append('MP = '+confs['mp']['val'].strip())
+        lines.append('DOS = '+confs['dos']['val'].strip())
+        lines.append('FPITCH = '+confs['fpitch']['val'].strip())
+        lines.append('SIGMA = '+confs['sigma']['val'].strip())
+        lines.append('')
+    
+        with open('band_dos.conf', 'w') as w:
+            w.write("\n".join(lines))
 
 #    def write_pwmat2phonopy_input():
 #        lines = []
