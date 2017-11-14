6485a1b4d2a6c41e848c490bc55f04202a56653f
PaulChern <cpiphy2011@gmail.com>
2017-11-14 15:24:24 +0800

    now read pwmat2phonopy.in

--- bin/PWmatRunPhonopy.py
+++ bin/PWmatRunPhonopy.py
@@ -20,22 +20,23 @@ if __name__ == "__main__":
     dir0 = os.getcwd()
 
     EtotInput = pwmat_run.InputParser(filename='etot.input')
+    pwmat2phonopy = pwmat_run.pwmat2phonopyParser(etotinput=EtotInput)
     confs_scf = EtotInput.get_configures()
     print('current workding directory is: '+dir0)
     print('phonon will be calculate in: '+dir0+'/phonon')
     if os.path.exists(dir0+'/pwmat2phonopy.in'):
-        raw_input('Please type enter to edit the \033[93m pwmat2phonopy.in \033[0m file:')
+        raw_input('\nPlease type enter to edit the \033[93m pwmat2phonopy.in \033[0m file\n')
         subprocess.call(["vim","./pwmat2phonopy.in"])
     else:
-        pwmat_run.pwmat2phonopyParser.write_input()
+        pwmat2phonopy.write_input()
+        raw_input('\nPlease type enter to edit the \033[93m pwmat2phonopy.in \033[0m file\n')
+        subprocess.call(["vim","./pwmat2phonopy.in"])
+    pwmat2phonopy.read_input(filename='pwmat2phonopy.in')
 
 #########################
 # produce displacements #
 #########################
-    DIM = raw_input('>> input supercell: "nx ny nz" (type enter to use default: "2 2 2") <<\n')
-    if DIM == '':
-        DIM = "2 2 2"
-        print("2 2 2")
+    DIM = pwmat2phonopy.get_configures('dim')['val'].strip()
     print(subprocess.Popen('PWmat2Phonopy --pwmat -d --dim="'+DIM+'" -c atom.config', shell=True, stdout=subprocess.PIPE).stdout.read())
 
 #######################
@@ -44,20 +45,12 @@ if __name__ == "__main__":
     os.chdir(dir0+'/phonon')
 
 # prepre nodes
-    nodes = raw_input('>> input node1 and node2 for supercell calculations: "node1 node2" (type enter to use default value the same as scf) <<\n')
-    if nodes != '':
-        EtotInput.set_configures('nodes', nodes)
-    else:
-        nodes = EtotInput.get_configures()['nodes']
-        print(nodes)
+    nodes = pwmat2phonopy.get_configures('nodes')['val']
+    EtotInput.set_configures('nodes', nodes.strip())
 
 # prepare new k-points for supercell
-    mp_n123 = raw_input('>> input new mp_n123 for the supercell: 10 10 10 0 0 0 (type enter to use default value the same as unitcell scf) <<\n')
-    if mp_n123 == '':
-        print(EtotInput.get_configures()['mp_n123'])
-        print("\033[93m\n WARNING: the same k-points as unitcell are used, the calculation for supercell will be heavy! \n\033[0m")
-    else:
-        EtotInput.set_configures('mp_n123', mp_n123.strip())
+    mp_n123 = pwmat2phonopy.get_configures('mp_n123')['val']
+    EtotInput.set_configures('mp_n123', mp_n123.strip())
 # prepare input files in the forces directory
     forces_dir = subprocess.Popen('ls -d forces*/ | cut -d "/" -f 1', shell=True, stdout=subprocess.PIPE).stdout.read()
     forces_dir = forces_dir.split()
@@ -73,40 +66,7 @@ if __name__ == "__main__":
 # prepare postprocess script
     pwmat_run.creat_post_process_script(num_forces=num_forces)
 
-    BAND = raw_input('>> input special Q point in the Brillioun zone: "Q1_x Q1_y Q1_z  Q2_x Q2_y Q2_z  Q3_x Q3_y Q3_z ......" \n (type enter to use default "0.0 0.0 0.0  0.5 0.0 0.0")<<\n\033[93m\n You can change it by modifying band_dos.conf after the forces are submitted to the cluster \n\033[0m')
-    if BAND == '':
-        BAND = "0.0 0.0 0.0  0.5 0.0 0.0"
-        print(BAND)
-    BAND_LABELS = raw_input('>> input special Q point symbol: "Q1  Q2  Q3 ......" \n (type enter to use default "\Gamma  X")<<\n\033[93m\n You can change it by modifying band_dos.conf after the forces are submitted to the cluster \n\033[0m')
-    if BAND_LABELS == '':
-        BAND_LABELS = "\Gamma  X"
-        print(BAND_LABELS)
-    BAND_POINTS = raw_input('>> input number of q points along the k path: "Num" (type enter to use default "101")<<\n\033[93m\n You can change it by modifying band_dos.conf after the forces are submitted to the cluster \n\033[0m')
-    if BAND_POINTS == '':
-        BAND_POINTS = '101'
-        print(BAND_POINTS)
-    unit = raw_input('>> Choose unit THz:1, cm^-1:2, eV:3, and meV:4, your choosen is : "number" (type enter to use default "1")<<\n\033[93m\n You can change it by modifying band_dos.conf after the forces are submitted to the cluster \n\033[0m')
-    if unit == '':
-        unit = '1'
-        print(unit)
-    MP = raw_input('>> input q-mesh for the DOS calculation: "num_q1 num_q2 num_q3" (type enter to use default "41 41 41")<<\n\033[93m\n You can change it by modifying band_dos.conf after the forces are submitted to the cluster \n\033[0m')
-    if MP == '':
-        MP = "41 41 41"
-        print(MP)
-    FPITCH = '0.1'
-    SIGMA = raw_input('>> input smearing width for the DOS calculation: "float_number" (type enter to use default "0.05")<<\n\033[93m\n You can change it by modifying band_dos.conf after the forces are submitted to the cluster \n\033[0m')
-    if SIGMA == '':
-        SIGMA = '0.05'
-        print(SIGMA)
-
-    pwmat_run.creat_phonopy_conf(DIM=DIM,
-                                 BAND=BAND,
-                                 BAND_LABELS=BAND_LABELS,
-                                 BAND_POINTS=BAND_POINTS,
-                                 unit=unit,
-                                 MP=MP,
-                                 FPITCH=FPITCH,
-                                 SIGMA=SIGMA)
+    pwmat2phonopy.creat_phonopy_conf()
 
     subprocess.Popen('chmod +x '+'plot_phonon.sh', shell=True)
 
@@ -115,10 +75,7 @@ if __name__ == "__main__":
     node2 = int(nodes.split()[1])
     ppn = node1*node2
     queue = 'test'
-    wall_time = raw_input('>> input wall time: "hours:minutes:seconds" (type enter to use default "1000:00:00")<<\n')
-    if wall_time == '':
-        wall_time = "1000:00:00"
-        print(wall_time)
+    wall_time = pwmat2phonopy.get_configures('wall_time')['val'].strip()
     for i in range(num_forces):
         force = "{pre_filename}-{0:0{width}}".format(i + 1, pre_filename='forces', width=3)
         os.chdir(dir0+'/phonon/'+force)
